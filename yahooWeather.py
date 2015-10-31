f7da5a83b0d4405b749468048e1c462d495b77dc
Josh Metz <me13tz@yahoo.com>
2015-10-31 16:17:01 -0700

    Get the latest weather for Seattle from Yahoo!
    Added the current temperature and tweaked the output a little to make it more readable.

--- yahooWeather.py
+++ yahooWeather.py
@@ -1,15 +1,22 @@
-#!/home/USERNAME/anaconda3/bin/python3.4
+#!/home/cassius/anaconda3/bin/python3.4
 import requests, bs4
 
+###download the weather report from Yahoo
 res = requests.get('https://weather.yahoo.com/united-states/washington/seattle-12798961/')
 if res.status_code != requests.codes.ok:
     print("May want to try downloading again - there was a problem.")
     exit()
-
+    
+###create a BeautifulSoup object and search for pertinent elements, assign variables
 wSoup = bs4.BeautifulSoup(res.text)
 p_tags = wSoup.select('p')
 today = str(p_tags[0])
 tomorrow = str(p_tags[1])
+span = wSoup.select('div span')
+x = str(span[8])
+print()
 
-print("Today's forecast from Yahoo Weather is:\n"+today[16:-4])
-print("Tomorrow's forecast from Yahoo Weather is:\n"+tomorrow[16:-4])
+###slice and dice for clean output
+print("Current temperature: "+x[34:36]+"\n")
+print("From Yahoo Weather:\n"+today[16:-4]+"\n")
+print(tomorrow[16:-4]+"\n")
